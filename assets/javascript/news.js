95f208ddee6d0f492697c0846d06327062534dd3
Tabebee <tabibone@gmail.com>
2017-10-11 01:02:59 -0400

    all files added. keys are hidden.

---
+++ assets/javascript/news.js
@@ -0,0 +1,33 @@
+//This was used to call the news API but it does not return information specific to the Premier League so was not used. Replaced with RSS feed widget.
+
+// var newsAPI = configAPI.newsKey;
+
+// $.ajax({
+// 	url:
+// 	"https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&" + newsAPI,
+// 	method: "GET",
+// 	error: function() {
+// 		console.log("we are fucked");
+// 	},
+// 	success: function(data) {
+// 		processData(data);
+// 	}
+// });
+
+// function processData(data) {
+// 	var articleItems = [];
+
+// 	for (var i = 0; i < data.articles.length; i++) {
+// 		var author = data.articles[i].author;
+// 		var title = data.articles[i].title;
+// 		var description = data.articles[i].description;
+// 		var artUrl = data.articles[i].url;
+
+// 		var $author = $('<div class="author"> Author: ' + author + "</div >");
+// 		var $title = $("<a href=" + artUrl + '> <div class="title">' + title + "</div> </a>");
+// 		var $description = $("<a href=" + artUrl +'> <div class="description">' + description + "</div> </a>");
+
+// 		$(".newsArticles").append($author, $title, $description);
+// 		console.log(artUrl);
+// 	}
+// }
---
+++ assets/javascript/weather.js
@@ -0,0 +1,43 @@
+var APIKey = configAPI.weatherKey;
+var geoLocation;
+var query = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+var teamStadiumLocation = [	"Bournemouth+BH7+7AF,+UK", "London+N7+7AJ,+UK", "Brighton+BN1+9BL,+UK", "Burnley+BB10+4BX,+UK",
+    "London+SW6+1HS,+UK", "London+SE25+6PU,+UK", "Liverpool+L4+4EL,+UK", " Huddersfield+HD1+6PG,+UK",
+    "Leicester+LE2+7FL,+UK", "Liverpool+L4+0TH,+UK", "Manchester+M11+3FF,+UK", "Manchester+M16+0RA,",
+    "Newcastle+upon+Tyne+NE1+4ST,+UK", "Southampton+SO14+5FP,+UK", "Stoke-on-Trent+ST4+4EG,+UK",
+    "Swansea+SA1+2FA,+UK", "London+N17+0AP,+UK", "Watford+WD18+0ER,+UK", "West+Bromwich+B71+4LF,+United Kingdom",
+    "London+E20+2ST,+UK"];
+
+
+
+// $(".thisTeamClass").on("click", function(){
+function getMyWeather (){
+    var queryURL = query + geoLocation + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response2) {
+
+        $(".temp").html("Temperature: " + response2.main.temp + " F");
+        $(".weather").html(response2.weather[0].description);
+        $(".wind").html("Wind Speed: " + response2.wind.speed + " mph");
+
+        console.log(response2);
+        //
+        //
+        // $(".temp").html("Temperature: " + response.main.temp + " F");
+        // $(".weather").html(response.weather[0].description);
+        // $(".wind").html("Wind Speed: " + response.wind.speed + " mph");
+        //
+        // console.log("Wind Speed: " + response.wind.speed + " mph");
+        // console.log("Weather: " + response.main.humidity);
+        // console.log("Temperature: " + response.main.temp + " F");
+    });
+}
+
+// console.log("this");
+// console.log();
+// });
+console.log("this");
+
